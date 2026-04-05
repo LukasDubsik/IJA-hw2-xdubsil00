1a1df775dd992e5d304825ba2ed422fe05a9b906
Lukas Dubsik <luke@bubaci.net>
2026-04-05 15:13:06 +0200

    feature(game): Implementation for the Terrain enum.
    This enum considers as valid four types: P,F,M,W

--- src/ija/ija2025/homework2/game/Terrain.java
+++ src/ija/ija2025/homework2/game/Terrain.java
@@ -1,5 +1,34 @@
 package ija.ija2025.homework2.game;
 
-public class Terrain {
-    
+public enum Terrain {
+    // List of the types and their names of the Terrain
+    PLAIN("P"),
+    FOREST("F"),
+    MOUNTAIN("M"),
+    WATER("W");
+
+    private String val;
+
+    Terrain(String val_) {
+        this.val = val_;
+    }
+
+    public static Terrain convert(String type) {
+        // Check that the input isn't null
+        if (type == null || type.trim().length() != 0) {
+            throw new IllegalArgumentException("Expected a single Letter input.");
+        }
+
+        // Attempt to normalie the string
+        String type_proc = type.trim().toUpperCase();
+
+        // Attempt to convert to the enum
+        return switch (type_proc) {
+            case "P" -> PLAIN;
+            case "F" -> FOREST;
+            case "M" -> MOUNTAIN;
+            case "W" -> WATER;
+            default -> throw new IllegalArgumentException("Unsupported terrain value: " + type_proc);
+        };
+    }
 }
