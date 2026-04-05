d029cb051dbfa5d193fb8cf2e06bff00f77f0f2e
Lukas Dubsik <luke@bubaci.net>
2026-04-05 14:37:16 +0200

    feature(game): Added base form to the GameFactory class.
    This class is static, this has private constructor.

--- src/ija/ija2025/homework2/game/GameFactory.java
+++ src/ija/ija2025/homework2/game/GameFactory.java
@@ -2,4 +2,12 @@ package ija.ija2025.homework2.game;
 
 public class GameFactory {
     
+    /**
+     * @brief Private constructor - static "class", makes it imposible to create as a class
+     */
+    private GameFactory() {
+
+    } 
+
+
 }
